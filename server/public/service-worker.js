47984da30ba5906a850dc137c92201e2705fe136
gplocanto <gplocanto@gmail.com>
2019-05-10 20:04:34 +0200

    build

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.0c230bf1743ed521c5fcea9e76b91676.js"
 );
 
 self.addEventListener('message', (event) => {
