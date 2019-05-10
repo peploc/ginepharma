1276165cb8d9f0e89c850c072f172817f7340ea4
gplocanto <gplocanto@gmail.com>
2019-05-10 20:13:55 +0200

    db

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ require('./passport/index');
 
 
 mongoose
-  .connect(process.env.DB_LOCAL, {useNewUrlParser: true})
+  .connect(process.env.DB_REMOTE, {useNewUrlParser: true})
   .then(x => { console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`) })
   .catch(err => { console.error('Error connecting to mongo', err) });
 
